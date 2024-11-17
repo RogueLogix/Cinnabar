c96547c469aee3f84e338906610c9385e87e0f21
RogueLogix <rogue@logix.dev>
2024-11-17 14:11:16 -0800

    Texture Atlas redirection

---
+++ src/main/java/graphics/cinnabar/internal/mixin/mixins/minecraft/texture/TextureAtlasMixin.java
@@ -0,0 +1,8 @@
+package graphics.cinnabar.internal.mixin.mixins.minecraft.texture;
+
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TextureAtlas.class)
+public class TextureAtlasMixin {
+}
