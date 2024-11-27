08c46346d95dbee6c1da72581d7c242a026f94df
RogueLogix <rogue@logix.dev>
2024-11-27 14:15:29 -0800

    noop automatic world screenshot

--- mod/src/main/java/graphics/cinnabar/internal/mixin/mixins/minecraft/renderer/GameRendererMixin.java
+++ mod/src/main/java/graphics/cinnabar/internal/mixin/mixins/minecraft/renderer/GameRendererMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceProvider;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -29,9 +30,14 @@ public class GameRendererMixin {
     private static ShaderInstance reloadShaders(ResourceProvider resourceProvider, String name, VertexFormat vertexFormat) throws IOException {
         return new CinnabarShaderInstance(resourceProvider, ResourceLocation.parse(name), vertexFormat);
     }
-
+    
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "setOrtho"))
     private static Matrix4f setOrtho0to1(Matrix4f matrix, float left, float right, float bottom, float top, float zNear, float zFar) {
         return matrix.setOrtho(left, right, bottom, top, zNear, zFar, true);
+    }
+    
+    @Overwrite
+    private void tryTakeScreenshotIfNeeded() {
+        // TODO: implement this properly
     }
 }
