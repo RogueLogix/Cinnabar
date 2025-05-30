59c1890cc94767f1447ebf9fc5752313179b57a0
RogueLogix <rogue@logix.dev>
2025-05-30 09:32:35 -0700

    handle oversize transient allocations

--- mod/src/core/java/graphics/cinnabar/core/vk/memory/pools/TransientPool.java
+++ mod/src/core/java/graphics/cinnabar/core/vk/memory/pools/TransientPool.java
@@ -7,6 +7,7 @@ import graphics.cinnabar.core.b3d.CinnabarDevice;
 import graphics.cinnabar.core.vk.memory.VkMemoryAllocation;
 import graphics.cinnabar.core.vk.memory.VkMemoryPool;
 import it.unimi.dsi.fastutil.longs.*;
+import it.unimi.dsi.fastutil.objects.ReferenceArrayList;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.vulkan.VkMemoryAllocateInfo;
 import org.lwjgl.vulkan.VkMemoryRequirements;
@@ -23,6 +24,9 @@ public class TransientPool implements VkMemoryPool, VkMemoryPool.Transient {
     protected final LongArrayList memoryBlocks = new LongArrayList();
     private int currentMemoryBlock = 0;
     private long currentMemoryBlockOffset = 0;
+    
+    protected final ReferenceArrayList<LongLongPair> liveOversizeAllocs = new ReferenceArrayList<>();
+    protected final ReferenceArrayList<LongLongPair> freeOversizeAllocs = new ReferenceArrayList<>();
     
     public TransientPool(CinnabarDevice device, int memoryType, long blockSize) {
         this.device = device;
@@ -46,39 +50,72 @@ public class TransientPool implements VkMemoryPool, VkMemoryPool.Transient {
         final var requiredSize = requirements.size();
         final var requiredAlignment = requirements.alignment();
         
-        for (int i = currentMemoryBlock; i < memoryBlocks.size(); i++) {
-            
-            final var startingOffset = (currentMemoryBlockOffset + requiredAlignment - 1) & -requiredAlignment;
-            final var endingOffset = startingOffset + requiredSize;
-            
-            if (endingOffset > blockSize) {
-                currentMemoryBlock++;
-                currentMemoryBlockOffset = 0;
-                continue;
+        if(requiredSize <= blockSize) {
+            for (int i = currentMemoryBlock; i < memoryBlocks.size(); i++) {
+                
+                final var startingOffset = (currentMemoryBlockOffset + requiredAlignment - 1) & -requiredAlignment;
+                final var endingOffset = startingOffset + requiredSize;
+                
+                if (endingOffset > blockSize) {
+                    currentMemoryBlock++;
+                    currentMemoryBlockOffset = 0;
+                    continue;
+                }
+                
+                currentMemoryBlockOffset = startingOffset;
+                break;
             }
-            
-            currentMemoryBlockOffset = startingOffset;
-            break;
+            if (currentMemoryBlock == memoryBlocks.size()) {
+                allocBlock(blockSize);
+            }
+            final var allocation = new LongObjectImmutablePair<>(memoryBlocks.getLong(currentMemoryBlock), new MemoryRange(currentMemoryBlockOffset, requiredSize));
+            currentMemoryBlockOffset += requiredSize;
+            return allocation;
+        } else {
+            final var alloc = oversizeAlloc(requiredSize);
+            liveOversizeAllocs.add(alloc);
+            return new LongObjectImmutablePair<>(alloc.secondLong(), new MemoryRange(0, requiredSize));
         }
-        
-        if (currentMemoryBlock == memoryBlocks.size()) {
-            allocBlock();
-        }
-        
-        final var allocation = new LongObjectImmutablePair<>(memoryBlocks.getLong(currentMemoryBlock), new MemoryRange(currentMemoryBlockOffset, requiredSize));
-        currentMemoryBlockOffset += requiredSize;
-        return allocation;
     }
     
-    protected void allocBlock() {
+    protected long allocBlock(long size) {
         try (final var stack = MemoryStack.stackPush()) {
             final var allocInfo = VkMemoryAllocateInfo.calloc(stack).sType$Default();
-            allocInfo.allocationSize(blockSize);
+            allocInfo.allocationSize(size);
             allocInfo.memoryTypeIndex(memoryType);
             final var memoryHandlePtr = stack.mallocLong(1);
             vkAllocateMemory(device.vkDevice, allocInfo, null, memoryHandlePtr);
-            memoryBlocks.add(memoryHandlePtr.get(0));
+            final var handle = memoryHandlePtr.get(0);
+            if (size == blockSize) {
+                memoryBlocks.add(handle);
+            } else {
+                // oversized alloc
+                freeOversizeAllocs.add(new LongLongImmutablePair(size, handle));
+            }
+            return handle;
         }
+    }
+    
+    protected LongLongPair oversizeAlloc(long size) {
+        for (int i = 0; i < freeOversizeAllocs.size(); i++) {
+            final var freeOversizeAlloc = freeOversizeAllocs.get(i);
+            if (freeOversizeAlloc.firstLong() == size) {
+                final var lastAlloc = freeOversizeAllocs.removeLast();
+                if(freeOversizeAlloc != lastAlloc){
+                    freeOversizeAllocs.set(i, lastAlloc);
+                }
+                return freeOversizeAlloc;
+            }
+        }
+        allocBlock(size);
+        return freeOversizeAllocs.removeLast();
+    }
+    
+    protected void freeUnusedOversizeAllocs() {
+        for (LongLongPair freeOversizeAlloc : freeOversizeAllocs) {
+            device.destroyEndOfFrame(() -> vkFreeMemory(device.vkDevice, freeOversizeAlloc.secondLong(), null));
+        }
+        freeOversizeAllocs.clear();
     }
     
     @Override
@@ -89,16 +126,19 @@ public class TransientPool implements VkMemoryPool, VkMemoryPool.Transient {
     
     @Override
     public long totalAllocatedFromVulkan() {
-        return memoryBlocks.size() * blockSize;
+        return memoryBlocks.size() * blockSize + liveOversizeAllocs.stream().mapToLong(LongLongPair::firstLong).sum() + freeOversizeAllocs.stream().mapToLong(LongLongPair::firstLong).sum() ;
     }
     
     @Override
     public long liveAllocated() {
-        return currentMemoryBlock * blockSize + currentMemoryBlockOffset;
+        return currentMemoryBlock * blockSize + currentMemoryBlockOffset + liveOversizeAllocs.stream().mapToLong(LongLongPair::firstLong).sum();
     }
     
     @Override
     public void reset() {
+        freeUnusedOversizeAllocs();
+        freeOversizeAllocs.addAll(liveOversizeAllocs);
+        liveOversizeAllocs.clear();
         currentMemoryBlock = 0;
         currentMemoryBlockOffset = 0;
     }
@@ -118,16 +158,25 @@ public class TransientPool implements VkMemoryPool, VkMemoryPool.Transient {
         }
         
         @Override
-        protected void allocBlock() {
-            super.allocBlock();
+        protected long allocBlock(long size) {
+            final var memoryHandle = super.allocBlock(size);
             try (final var stack = MemoryStack.stackPush()) {
                 final var hostPtr = stack.mallocPointer(1);
-                final var memoryHandle = memoryBlocks.peekLong(0);
                 vkMapMemory(device.vkDevice, memoryHandle, 0, blockSize, 0, hostPtr);
                 final var mappedRange = new PointerWrapper(hostPtr.get(0), blockSize);
                 LeakDetection.addAccessibleLocation(mappedRange);
                 mappedMemory.put(memoryHandle, new PointerWrapper(hostPtr.get(0), blockSize));
             }
+            return memoryHandle;
+        }
+        
+        @Override
+        protected void freeUnusedOversizeAllocs() {
+            for (LongLongPair freeOversizeAlloc : freeOversizeAllocs) {
+                final var handle = freeOversizeAlloc.secondLong();
+                LeakDetection.removeAccessibleLocation(mappedMemory.remove(handle));
+            }
+            super.freeUnusedOversizeAllocs();
         }
         
         @Override
