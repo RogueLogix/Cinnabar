1da832476da5dfff0dc914f0043592ac73053815
RogueLogix <rogue@logix.dev>
2025-06-10 21:14:04 -0700

    fix dev env and inline all libs allows for early window to be implemented properly

--- loader/src/main/java/graphics/cinnabar/services/CinnabarLocator.java
+++ loader/src/main/java/graphics/cinnabar/services/CinnabarLocator.java
@@ -45,8 +45,7 @@ public class CinnabarLocator implements IModFileCandidateLocator {
                     return;
                 }
                 LOGGER.debug("Loading mod {}", filename);
-                final var parent = pipeline.addJarContent(JarContents.of(path), ModFileDiscoveryAttributes.DEFAULT, IncompatibleFileReporting.WARN_ALWAYS).get();
-                loadLibs(pipeline, parent);
+                pipeline.addJarContent(JarContents.of(path), ModFileDiscoveryAttributes.DEFAULT, IncompatibleFileReporting.WARN_ALWAYS);
             });
         } catch (Exception e) {
             LOGGER.error("Fatal error encountered locating Cinnabar jars", e);
@@ -54,30 +53,6 @@ public class CinnabarLocator implements IModFileCandidateLocator {
                 throw loadingException;
             }
             throw new ModFileLoadingException(e.getMessage());
-        }
-    }
-    
-    private static void loadLibs(IDiscoveryPipeline pipeline, IModFile parent) {
-        try {
-            final var nativesSuffix = "natives-" + switch (Platform.get()) {
-                case LINUX -> "linux";
-                case MACOSX -> "macos";
-                case WINDOWS -> "windows";
-            } + (Platform.getArchitecture() == Platform.Architecture.ARM64 ? "-arm64" : "") + ".jar";
-            walk(Path.of(CinnabarLocator.class.getResource("/META-INF/lib/").toURI()), 1).forEach(path -> {
-                final var filename = path.getFileName().toString();
-                if (!filename.endsWith(".jar")) {
-                    return;
-                }
-                if (filename.contains("natives") && !filename.endsWith(nativesSuffix)) {
-                    // dont load natives for different platforms
-                    return;
-                }
-                LOGGER.debug("Loading lib {}", filename);
-                pipeline.addModFile(IModFile.create(SecureJar.from(path), JarModsDotTomlModFileReader::manifestParser, IModFile.Type.LIBRARY, ModFileDiscoveryAttributes.DEFAULT.withParent(parent)));
-            });
-        } catch (Exception e) {
-            LOGGER.error("Fatal error encountered locating Cinnabar lib jars", e);
         }
     }
 }
