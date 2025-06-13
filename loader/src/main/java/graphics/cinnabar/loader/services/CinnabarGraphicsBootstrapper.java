61c4660ed6d9068512ac43de38e4e1b5e18c33d9
RogueLogix <rogue@logix.dev>
2025-06-13 10:25:18 -0700

    add compatibility range check to bootstrapper before Cinnabar attempts to load. ensures that if Neo is going to fail the mod's version check later, it will show the correct error screen instead of crashing.

--- loader/src/main/java/graphics/cinnabar/loader/services/CinnabarGraphicsBootstrapper.java
+++ loader/src/main/java/graphics/cinnabar/loader/services/CinnabarGraphicsBootstrapper.java
@@ -2,7 +2,13 @@ package graphics.cinnabar.loader.services;
 
 import com.mojang.logging.LogUtils;
 import net.neoforged.neoforgespi.earlywindow.GraphicsBootstrapper;
+import org.apache.maven.artifact.versioning.ComparableVersion;
 import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class CinnabarGraphicsBootstrapper implements GraphicsBootstrapper {
     private static final Logger LOGGER = LogUtils.getLogger();
@@ -18,7 +24,32 @@ public class CinnabarGraphicsBootstrapper implements GraphicsBootstrapper {
     
     @Override
     public void bootstrap(String[] arguments) {
-        CinnabarEarlyWindowProvider.attemptConfigInit();
-        CinnabarLaunchPlugin.attemptInject();
+        for (int i = 0; i < arguments.length - 1; i++) {
+            if ("--fml.neoForgeVersion".equals(arguments[i])) {
+                if (!neoVersionSupported(arguments[i + 1])) {
+                    // don't init cinnabar's early window with an incompatible neo version, the mod itself will fail the loading later
+                    return;
+                }
+                CinnabarEarlyWindowProvider.attemptConfigInit();
+                CinnabarLaunchPlugin.attemptInject();
+            }
+        }
+    }
+    
+    private static boolean neoVersionSupported(String neoVersion) {
+        final String[] versionBounds;
+        try {
+            final var resource = CinnabarLocator.class.getResource("/modinfo.toml");
+            assert resource != null;
+            final var lines = Files.readAllLines(Path.of(resource.toURI()));
+            final var neoVersionString = lines.getFirst().split("=")[1];
+            versionBounds = neoVersionString.substring(2, neoVersionString.length() - 2).split(",");
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        final var lowerBound = new ComparableVersion(versionBounds[0]);
+        final var upperBound = new ComparableVersion(versionBounds[1]);
+        final var currentVersion = new ComparableVersion(neoVersion);
+        return lowerBound.compareTo(currentVersion) <= 0 && currentVersion.compareTo(upperBound) < 0;
     }
 }
